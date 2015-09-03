72689e17b9163d8260ca9243702c55327479ec60
Michael.Gollner <Michael.Gollner@NB03-Neu.office.local>
2015-09-03 13:47:14 +0200

    - Multi language support added  - English  - German
    - some little code formattings
    - changed comments from german to english and use english as development language

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 Changelog:
 
+Version 1.2
+- Multi language support added
+ - English
+ - German
+
+- some little code formattings
+- changed comments from german to english and use english as development language
+
 Version 1.1
 3D tag sphere plugin for Joomla 3.x based on jQuery Tagcloud Plugin
 
--- tagSphere.php
+++ tagSphere.php
@@ -2,19 +2,19 @@
 
 /**
  * plugin plgContentTagSphere
- * @version 1.1
+ * @version 1.2
  * @package plgContentTagSphere
  * @copyright Copyright (c) 2015 Michael Gollner www.it-mg.net
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0 GNU/GPL Version 2.0
  */
 /**
- * Platz für Informationen
+ * Informations
  * =======================
  *
- * Anwendung im Content:
+ * Usage in content:
  *   {TagSphere}
  *
- * Anwendung im Content mit Parameterübergabe:
+ * Usage in content with paramters:
  *   {TagSphere param1=Hello!|param2=it works fine|param3=Joomla! rocks ;-)}
  */
 defined('_JEXEC') or die;
@@ -22,17 +22,14 @@ defined('_JEXEC') or die;
 jimport('joomla.plugin.plugin');
 
 class plgContentTagSphere extends JPlugin {
-    
- 
 
-    function plgContentTagSphere(&$subject,$params) {
-        parent::__construct($subject,$params);
-       
+    function plgContentTagSphere(&$subject, $params) {
+        parent::__construct($subject, $params);
     }
 
     /**
-     * Contentstring Definition
-     * String erkennen und mit neuem Inhalt füllen
+     * Contentstring definition
+     * Get string and fill it with new data
      */
     public function onContentPrepare($context, &$article, &$params, $limitstart) {
         $regex = '/{TagSphere\s*(.*?)}/i';
@@ -43,88 +40,76 @@ class plgContentTagSphere extends JPlugin {
     public function form($matches) {
 
         /**
-         * Contentstring zerlegen
+         * Split contentstring 
          */
         $string = $matches[1];
         $params = explode('|', $string);
 
-
-        
-        /**
-         * individuelle Anwendung starten
-         */
-        
-        if (isset($parameter1)) {
-            $output .= "<h4>" . $parameter1 . "</h4>";
-        }
-       
-        
+        //create JS output
         $output = $this->createJS();
-         $output .= ' <div id="tagcloud" style="color: ' . $this->params->get('fontcolor') .'; background-color: ' . $this->params->get('bgcolor','transparent') . ';">
+
+        //extend output with the tagcloud html div tag
+        $output .= ' <div id="tagcloud" style="color: ' . $this->params->get('fontcolor') . '; background-color: ' . $this->params->get('bgcolor', 'transparent') . ';">
         <ul>';
-        
+
         /**
-         * Parameter raus filtern und Variablen erstellen
+         * Filter params and create variables
          */
         foreach ($params as $param) {
-            
-            //  <li><a href="#" style="color: ' . $this->params->get('fontcolor') . ';">dazzlingly</a></li>
-            
+
             if (stristr($param, 'tags=')) {
                 $parameter1 = str_replace('tags=', '', $param);
-                
+
                 $tags = explode(',', $parameter1);
-                foreach ($tags as $tag)
-                {
+                foreach ($tags as $tag) {
                     $output .= ' <li><a href="#" style="color: ' . $this->params->get('fontcolor') . ';">' . trim($tag) . '</a></li>';
                 }
-                
             }
-            /*if (stristr($param, 'param2=')) {
-                $parameter2 = str_replace('param2=', '', $param);
-            }
-            if (stristr($param, 'param3=')) {
-                $parameter3 = str_replace('param3=', '', $param);
-            }*/
-            // kann beliebig erweitert werden
+            /* if (stristr($param, 'param2=')) {
+              $parameter2 = str_replace('param2=', '', $param);
+              }
+              if (stristr($param, 'param3=')) {
+              $parameter3 = str_replace('param3=', '', $param);
+              } */
+            // extend if you want
         }
-        
-        
-        
-          
-            
-        $output .=  '</ul></div>';
+
+
+
+
+
+        $output .= '</ul></div>';
 
 
         return $output;
     }
 
-    
     protected function createJS() {
-        
+
         $output = "";
-        if($this->params->get('usejQuery'))
+        if ($this->params->get('usejQuery'))
             $output .= '<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>';
-        
+
         $output .= '<script type="text/javascript" src="' . JUri::base() . 'plugins/content/tagSphere/js/tagcloud.jquery.min.js"></script>';
-        $output .= '<script type="text/javascript" >';        
+        $output .= '<script type="text/javascript" >';
         $output .= 'var settings = {';
-        $output .= 'height: ' . $this->params->get('height') . 
-                ',width: ' . $this->params->get('height') . 
-                ',radius: ' . $this->params->get('radius') . 
-                ',speed: ' . $this->params->get('speed') . 
-                ',slower: 0.9, timer: 5,fontMultiplier: '. $this->params->get('fontMultiplier') . ',';
+        $output .= 'height: ' . $this->params->get('height') .
+                ',width: ' . $this->params->get('height') .
+                ',radius: ' . $this->params->get('radius') .
+                ',speed: ' . $this->params->get('speed') .
+                ',slower: 0.9, timer: 5,fontMultiplier: ' . $this->params->get('fontMultiplier') . ',';
         $output .= 'hoverStyle: {border: "none", color: "#0b2e6f" },';
         $output .= 'mouseOutStyle: { border: "", color: "" }};';
-        
+
         $output .= 'jQuery(document).ready(function(){';
         $output .= "jQuery('#tagcloud').tagoSphere(settings);";
         $output .= '});';
-        
-        
+
+
         $output .= '</script>';
         return $output;
     }
+
 }
 
 ?>
